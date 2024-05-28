165a28d2208813439a412f660d4ede5fa588468e
RUTMIR1 <theaxeblue@gmail.com>
2024-05-27 22:29:46 -0300

    Cambios finales del funcionamiento ABM de Atleta

--- ClasesBase/Properties/Settings.Designer.cs
+++ ClasesBase/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace ClasesBase.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\APU 2024\\Repositorios\\Nue" +
-            "vo\\comdep.mdf\";Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\RUTMIR\\source\\repos\\" +
+            "LPOOI_Grupo_10\\comdep.mdf;Integrated Security=True;Connect Timeout=30")]
         public string comdepConnectionString {
             get {
                 return ((string)(this["comdepConnectionString"]));
